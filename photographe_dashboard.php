6c2a73761f703f00f3060d38e44b26ec0c6aa358
Leo Letanneur <leo.letanneur@exemple.com>
2025-11-18 13:46:04 +0100

    Ajout du footer commun sur les dashboards (admin, photographe, client)

--- photographe_dashboard.php
+++ photographe_dashboard.php
@@ -487,6 +487,7 @@ try {
         </div>
     </div>
 
+    <?php include 'footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
